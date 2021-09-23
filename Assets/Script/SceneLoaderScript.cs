1fe5e76f5d6632ec3d95bf193f4936395093a231
umaishasan <umaishasan96@gmail.com>
2021-09-23 18:20:08 +0500

    new working

---
+++ Assets/Script/SceneLoaderScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderScript : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime;
+
+    public void SceneLoad()
+    {
+
+        StartCoroutine(SceneNextInDelay(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator SceneNextInDelay(int SceneIndex)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(SceneIndex);
+    }
+}
